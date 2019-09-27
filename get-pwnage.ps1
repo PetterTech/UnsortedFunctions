6bb4a54b719c13ea877652edeb0464ef46dbaaf6
nerenther <parnesen@hotmail.com>
2019-09-27 10:03:53 +0200

    initial commit

---
+++ get-pwnage.ps1
@@ -0,0 +1,46 @@
+﻿function Get-Pwnage
+    {
+        [CmdletBinding()] 
+            Param (
+                [Parameter(Mandatory=$True,Position=0)][string]$Emailaddress,
+                [Parameter(Mandatory=$True,Position=1,)][string]$APIkey,
+                [switch]$Allinfo
+                  )
+    Begin {
+        $Header = @{
+            "hibp-api-key" = "d5c3ff76ac974191a7209cbfa7c9548d"
+        }
+    }
+
+    Process {
+        if ($Allinfo) {
+            $Response = Invoke-RestMethod -Uri "https://haveibeenpwned.com/api/v3/breachedaccount/$($Emailaddress)?truncateResponse=false" -Method Get -Headers $Header
+        }
+
+        else {
+            $Response = Invoke-RestMethod -Uri "https://haveibeenpwned.com/api/v3/breachedaccount/$($Emailaddress)" -Method Get -Headers $Header
+        }
+    }
+
+    End {
+        $Response
+    }
+    
+        
+<#
+ .Synopsis
+  Short description
+ .Description
+  Long description
+ .Parameter Parameter1
+  Info about Parameter1
+ .Example
+  verb-noun
+  Describe what verb-noun does
+ .Example
+  verb-noun -Parameter1 something
+  Describe what verb-noun -Parameter1 something does
+ .Link
+  http://cloud.kemta.net
+ #>
+}
---
+++ verb-noun(template).ps1
@@ -0,0 +1,42 @@
+﻿function verb-noun
+    {
+        [CmdletBinding()] 
+            Param (
+                [Parameter(Mandatory=$True,Position=0,HelpMessage='Lorem Ipsum')][string]$Mandatoryparameter,
+                [string]$Presetparameter = 'value',
+                [string]$StringParameter,
+                [ValidateScript({Get-MsolPartnerContract -DomainName $_})][string]$ValidatedParameter,
+                [int]$IntegerParameter,
+                $WeeklyTypedParameter,
+                [switch]$Switch
+                  )
+    Begin {
+	
+        }
+
+    Process {
+        code
+        }
+
+    End {
+	
+        }
+    
+        
+<#
+ .Synopsis
+  Short description
+ .Description
+  Long description
+ .Parameter Parameter1
+  Info about Parameter1
+ .Example
+  verb-noun
+  Describe what verb-noun does
+ .Example
+  verb-noun -Parameter1 something
+  Describe what verb-noun -Parameter1 something does
+ .Link
+  http://cloud.kemta.net
+ #>
+}
